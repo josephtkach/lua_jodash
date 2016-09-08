7263730f5c95593b415e29508a4576fd8be8c0a6
Joseph Tkach <josephtkach@gmail.com>
2016-09-08 09:39:30 -0700

    sortedLastIndexOf

--- tests/sortedLastIndexOf.lua
+++ tests/sortedLastIndexOf.lua
@@ -1,33 +1,33 @@
 -------------------------------------------------------------------------------
-local test = junit:new({ name = "Array:SortedIndexOf" })
+local test = junit:new({ name = "Array:SortedLastIndexOf" })
 
 -------------------------------------------------------------------------------
 function test.OneToTen(data)
     for i,v in ipairs(data.ten) do
-        expect( jo.sortedIndexOf(data.ten, v) ):toBe(i)
+        expect( jo.sortedLastIndexOf(data.ten, v) ):toBe(i)
     end
 end
 
 -------------------------------------------------------------------------------
 function test.WithDuplicate(data)
     local input = { 1,2,3,3,4,5 }
-    expect( jo.sortedIndexOf(input, 3) ):toBe(3)
+    expect( jo.sortedLastIndexOf(input, 3) ):toBe(4)
 end
 
 -------------------------------------------------------------------------------
 function test.TableSizeOne(data)
-    expect( jo.sortedIndexOf({ 1 }, 1) ):toBe(1)
+    expect( jo.sortedLastIndexOf({ 1 }, 1) ):toBe(1)
 end
 
 -------------------------------------------------------------------------------
 function test.Empty(data)
-    expect( jo.sortedIndexOf(data.empty, 1) ):toBe(-1)
+    expect( jo.sortedLastIndexOf(data.empty, 1) ):toBe(-1)
 end
 
 -------------------------------------------------------------------------------
 function test.AllTheSameValue(data)
     local input = { 10, 10, 10, 10, 10 }
-    expect( jo.sortedIndexOf(input, 10) ):toBe(1)
+    expect( jo.sortedLastIndexOf(input, 10) ):toBe(5)
 end
 
 
